50c6d0475112e0e4060f1d02db35b9a178bda97d
Christina <150205323+berryalien@users.noreply.github.com>
2024-09-02 23:35:03 +0400

    added logic to main.js for the accordion block

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const menuBtn = document.querySelector('.menu__btn');
 const menuMobile = document.querySelector('.header__menu-list');
 
 menuBtn.addEventListener('click', () => {
-menuMobile.classList.toggle('menu--open');
+  menuMobile.classList.toggle('menu--open');
 });
 
 const swiperOne = new Swiper('.feedback__slider', {
@@ -21,12 +21,22 @@ const swiperTwo = new Swiper('.certificates__slider', {
   },
 });
 
-document.querySelectorAll('.accordeon__triger').forEach((item) => {
-  item.addEventListener('click', () => {
-    item.parentNode.classList.toggle('accordeon__item--active')
-  })
-});
+const accordeon = document.querySelector('.accordeon');
+const accordeonTitles = document.querySelectorAll('.accordeon__title');
 
-foo.onclick = function () {
-  this.classList.toggle('rotate')
-}
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener('click', function () {
+
+    const currentText = accordeonTitle.parentElement.querySelector('.accordeon__text');
+
+    accordeonTitle.classList.toggle('accordeon__title--active');
+    currentText.classList.toggle('accordeon__text--visible');
+
+    if (currentText.classList.contains('accordeon__text--visible')) {
+      currentText.style.maxHeight = currentText.scrollHeight + 'px'
+    } else {
+      currentText.style.maxHeight = null
+    }
+  });
+});
